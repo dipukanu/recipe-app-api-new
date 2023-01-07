abfd835a31ebab475909a9be03758c1feced99d2
dipukanu96 <dipu.kanu96@healthosbd.com>
2023-01-07 23:15:36 +0600

    Perform Simple Test

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtact_numberrs(self):
+        """Test subtract numbers"""
+        res = calc.subtract(15, 10)
+
+        self.assertEqual(res, 5)
