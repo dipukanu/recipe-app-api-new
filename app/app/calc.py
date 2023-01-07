88de7f8d435b86a63e059c0bb12ded181a8852a5
dipukanu96 <dipu.kanu96@healthosbd.com>
2023-01-07 23:18:55 +0600

    Updated code for linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,4 @@ def add(x, y):
 
 def subtract(x, y):
     return x-y
+    
--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ from app import calc
 class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
